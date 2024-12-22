29420871c04bd625c1a180161bd40b435fcbaf43
HD88-max <hardikdhallam@gmail.com>
2024-12-22 16:14:05 +0000

    Add files via upload

---
+++ printing.py
@@ -0,0 +1,5 @@
+print("Hello")
+ 
+print("welcome to earth" ,2)
+print("what is your name" ,end="?")
+print("please state")
---
+++ variables.py
@@ -0,0 +1,10 @@
+x = 12
+y = 45
+
+print(x)
+print(y)
+
+print(x,"plus",y, "is equal to", x + y)
+
+name = input("what is your name ")
+print("Hello" , name)
